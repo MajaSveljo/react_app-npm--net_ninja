64142c3ca5006758a8bbd2c439be1dec6fe1c0ca
MajaSveljo <majasveljo@gmail.com>
2018-11-01 13:29:04 +0100

    if and ternary (conditional) output n.16

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,18 +1,49 @@
 import React from 'react';
 
 const Ninjas = ({ninjas}) => {
-    const ninjaList = ninjas.map(ninja => {
-        return (
-            <div className="ninja" key={ninja.id}>
-                <div>{ninja.name}</div>
-                <div>{ninja.age}</div>
-                <div>{ninja.belt}</div>
-            </div>
-        )
-    })
+    //regular if statement
+
+    // const ninjaList = ninjas.map(ninja => {
+    //     if (ninja.age > 20) {
+    //         return (
+    //             <div className="ninja" key={ninja.id}>
+    //                 <div>{ninja.name}</div>
+    //                 <div>{ninja.age}</div>
+    //                 <div>{ninja.belt}</div>
+    //             </div>
+    //         )
+    //     } else {
+    //         return null
+    // //     }
+    // })
+
+    //ternary solution
+
+    // const ninjaList = ninjas.map(ninja => {
+    //     // condition ? (returns if true) : (returns if false)
+    //     return ninja.age > 20 ? (
+    //         <div className="ninja" key={ninja.id}>
+    //             <div>{ninja.name}</div>
+    //             <div>{ninja.age}</div>
+    //             <div>{ninja.belt}</div>
+    //         </div>             
+    //     ) : null
+    //     //this is one way. it can also be output (nested) directly
+    //     //in the template
+    // });
     return(
         <div className="ninja-list">
-            {ninjaList}
+            {
+                ninjas.map(ninja => {
+                    return ninja.age > 20 ? (
+                        <div className="ninja" key={ninja.id}>
+                            <div>{ninja.name}</div>
+                            <div>{ninja.age}</div>
+                            <div>{ninja.belt}</div>
+                        </div>             
+                    ) : null;
+            })
+            }
         </div>
     )
 }
