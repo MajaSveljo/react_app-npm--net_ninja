d98f6a93d1841c8c008f905c0525762a7de274d3
MajaSveljo <majasveljo@gmail.com>
2018-11-01 14:14:32 +0100

    changing state by props n.18

--- src/App.js
+++ src/App.js
@@ -10,13 +10,20 @@ class App extends Component {
       {name: 'Pepsi', age:2, belt: 'brown', id:3}
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
         <p>Welcome :D</p>
         <Ninjas ninjas={this.state.ninjas}/>
-        <AddNinja />
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     );
   }
